474e592219fc4a259c03fbcfbd10eec7eb9ad9da
Tamas <lukacstamas1114@gmail.com>
2024-03-07 14:02:28 +0100

    Data Vorbereitung wurde erweitert

--- data_prep.py
+++ data_prep.py
@@ -16,6 +16,7 @@ from sklearn.linear_model import LinearRegression
 import re
 from itertools import islice
 from datetime import datetime
+import struct
 
 # Import Funktionen aus anderen Modulen
 from database import upload_file
@@ -24,6 +25,58 @@ from database import upload_file
 def convert_dataset_to_list(dataset):
     data = dataset[:]
     return list(data)
+
+def convert_data_types(distance, magnetization,velocity,timestamp,wall_thickness, i, dataset):
+    try:
+        if isinstance(distance, bytes):
+            try: 
+                distance=float(distance.decode('utf-8'))
+            except:
+                distance=dataset['distance'][i-1]
+                distance=float(distance.decode('utf-8'))
+    except (ValueError, OverflowError):
+        print(distance)
+    try:
+        if isinstance(magnetization, bytes):
+            try:
+                magnetization=float(magnetization.decode('utf-8'))
+            except:
+                magnetization=dataset['magnetization'][i-1]
+                magnetization=float(magnetization.decode('utf-8'))
+    except:
+        pass
+    try:
+        if isinstance(velocity, bytes):
+            try:
+                velocity=float(velocity.decode('utf-8'))
+            except:
+                velocity=dataset['velocity'][i-1]
+                velocity=float(velocity.decode('utf-8'))
+    except:
+        pass
+    try:
+        if isinstance(wall_thickness, bytes):
+            try:
+                wall_thickness=float(wall_thickness.decode('utf-8'))
+            except:
+                wall_thickness=dataset['wall_thickness'][i-1]
+                wall_thickness=float(wall_thickness.decode('utf-8'))
+    except:
+        pass
+
+    # Versuch, den Timestamp zu dekodieren und in einen float umzuwandeln
+    try:
+        timestamp = timestamp.decode()
+    except:
+        pass
+
+    # Wenn der Timestamp ein String ist, wird er in ein datetime-Objekt umgewandelt und dann in einen Timestamp
+    try:
+        timestamp = datetime.strptime(str(timestamp), "%Y-%m-%dT%H:%M:%S")
+        timestamp = timestamp.timestamp()
+    except:
+        pass
+    return distance, magnetization,timestamp
 
 # Funktion um die Bytes Datensätze in zu encodierte JSON-Strings umzuwandeln
 def convert_dataset_bytes_to_json(dataset):
@@ -106,52 +159,39 @@ def convert_h5_to_json(file_path):
         avg_magnetization=0
         avg_velocity=0
         avg_wall_thickness=0
-
+        comp_magnetization = []
         # Datensätze in ein neues Dictionary umwandeln
         for i, (defect_channel,distance, magnetization,timestamp,velocity,wall_thickness) in \
             enumerate(zip(dataset['defect_channel'], dataset['distance'], dataset['magnetization'], \
                           dataset['timestamp'], dataset['velocity'], dataset['wall_thickness'])):
             
+            """# Wenn der Datensatz Eastereggs enthält, werden die Werte durch den Durchschnitt ersetzttry:
+            try:
+                if isinstance(distance, bytes):
+                    decoded_distance = distance.decode('utf-8')
+                    if 'Easteregg' in decoded_distance:
+                        print('Easteregg found in distance')
+                        distance = dataset['distance'][j-1]
+                if isinstance(magnetization, bytes):
+                    decoded_magnetization = magnetization.decode('utf-8')
+                    if 'Easteregg' in decoded_magnetization:
+                        print('Easteregg found in magnetization')
+                        magnetization = dataset['magnetization'][j-1]
+                if isinstance(velocity, bytes):
+                    decoded_velocity = velocity.decode('utf-8')
+                    if 'Easteregg' in decoded_velocity:
+                        print('Easteregg found in velocity')
+                        velocity = dataset['velocity'][j-1]
+                if isinstance(wall_thickness, bytes):
+                    decoded_wall_thickness = wall_thickness.decode('utf-8')
+                    if 'Easteregg' in decoded_wall_thickness:
+                        print('Easteregg found in wall_thickness')
+                        wall_thickness = dataset['wall_thickness'][j-1]
+            except Exception as e:
+                print(f"An error occurred: {e}")"""
+
             # Wenn der Datensatz Bytes enthält, wird die Funktion convert_dataset_bytes_to_json() aufgerufen, um den Datensatz in einen encodierten JSON-String umzuwandeln
-            try:
-                if type(distance)==bytes:
-                    distance=int.from_bytes(distance)
-            except:
-                pass
-
-            # Wenn der Datensatz Eastereggs enthält, werden die Werte durch den Durchschnitt ersetzt
-            try:
-                if re.search(r'Easter', distance):
-                    distance=avg_distance
-                if re.search(r'Easter', magnetization):
-                    magnetization=avg_magnetization
-                if re.search(r'Easter', velocity):
-                    velocity=avg_velocity
-                if re.search(r'Easter', wall_thickness):
-                    wall_thickness=avg_wall_thickness
-            except:
-                pass
-            # Durchschnitt der Werte berechnen
-            try:
-                avg_distance=(avg_distance+int(distance))/i
-                avg_magnetization=(avg_magnetization+int(magnetization))/i
-                avg_velocity=(avg_velocity+int(velocity))/i
-                avg_wall_thickness=(avg_wall_thickness+int(wall_thickness))/i
-            except:
-                pass
-
-            # Versuch, den Timestamp zu dekodieren und in einen float umzuwandeln
-            try:
-                timestamp=timestamp.decode()
-            except:
-                pass
-
-            # Wenn der Timestamp ein String ist, wird er in ein datetime-Objekt umgewandelt und dann in einen Timestamp
-            try:
-                timestamp = datetime.strptime(str(timestamp), "%Y-%m-%dT%H:%M:%S")
-                timestamp=timestamp.timestamp()
-            except:
-                pass
+            distance, magnetization,timestamp = convert_data_types(distance, magnetization, velocity,timestamp, wall_thickness, i, dataset)
 
             # Wenn der Loop zum ersten Mal durchläuft, wird der Wert von timestamp in der Variable time gespeichert und Timestamp auf 0 gesetzt
             if i==0:
@@ -167,7 +207,7 @@ def convert_h5_to_json(file_path):
                 "punkt": i,
                 "defect_channel": defect_channel,
                 "distance": distance,
-                "magnetization": magnetization,
+                "magnetization": abs(magnetization),
                 "timestamp": timestamp,
                 "velocity": velocity,
                 "wall_thickness": wall_thickness,
@@ -182,43 +222,33 @@ def convert_h5_to_json(file_path):
         if anz!=1000: # Wenn die Anzahl der Datensätze nicht 1000 beträgt, werden die fehlenden Datensätze ausgerechnet und in die Liste eingefügt
             for (defect_channel,distance, magnetization,timestamp,wall_thickness) in \
             islice(zip(dataset['defect_channel'], dataset['distance'], dataset['magnetization'], dataset['timestamp'], dataset['wall_thickness']),j,None):
-                
-                # Wenn der Datensatz Bytes enthält, wird die Funktion convert_dataset_bytes_to_json() aufgerufen, um den Datensatz in einen encodierten JSON-String umzuwandeln
-                if type(distance)==bytes:
-                    distance=int.from_bytes(distance)
-
                 # Wenn der Datensatz Eastereggs enthält, werden die Werte durch den Durchschnitt ersetzt
-                try:
-                    if re.search(r'Easter', distance):
-                        distance=avg_distance
-                    if re.search(r'Easter', magnetization):
-                        magnetization=avg_magnetization
-                    if re.search(r'Easter', velocity):
-                        velocity=avg_velocity
-                    if re.search(r'Easter', wall_thickness):
-                        wall_thickness=avg_wall_thickness
-                except:
-                    pass
-                # Durchschnitt der Werte berechnen
-                try:
-                    avg_distance=(avg_distance+int(distance))/i
-                    avg_magnetization=(avg_magnetization+int(magnetization))/i
-                    avg_velocity=(avg_velocity+int(velocity))/i
-                    avg_wall_thickness=(avg_wall_thickness+int(wall_thickness))/i
-                except:
-                    pass
+                """try:
+                    if isinstance(distance, bytes):
+                        decoded_distance = distance.decode('utf-8')
+                        if 'Easteregg' in decoded_distance:
+                            print('Easteregg found in distance')
+                            distance = dataset['distance'][j-1]
+                    if isinstance(magnetization, bytes):
+                        decoded_magnetization = magnetization.decode('utf-8')
+                        if 'Easteregg' in decoded_magnetization:
+                            print('Easteregg found in magnetization')
+                            magnetization = dataset['magnetization'][j-1]
+                    if isinstance(velocity, bytes):
+                        decoded_velocity = velocity.decode('utf-8')
+                        if 'Easteregg' in decoded_velocity:
+                            print('Easteregg found in velocity')
+                            velocity = dataset['velocity'][j-1]
+                    if isinstance(wall_thickness, bytes):
+                        decoded_wall_thickness = wall_thickness.decode('utf-8')
+                        if 'Easteregg' in decoded_wall_thickness:
+                            print('Easteregg found in wall_thickness')
+                            wall_thickness = dataset['wall_thickness'][j-1]
+                except Exception as e:
+                    print(f"An error occurred: {e}")"""
 
-                # Versuch, den Timestamp zu dekodieren und in einen float umzuwandeln
-                try:
-                    timestamp=timestamp.decode()
-                except:
-                    pass
-                
-                try:
-                    timestamp = datetime.strptime(str(timestamp), "%Y-%m-%dT%H:%M:%S")
-                    timestamp=timestamp.timestamp()
-                except:
-                    pass
+                # Wenn der Datensatz Bytes enthält, wird die Funktion convert_dataset_bytes_to_json() aufgerufen, um den Datensatz in einen encodierten JSON-String umzuwandeln
+                distance, magnetization,timestamp = convert_data_types(distance, magnetization, velocity,timestamp, wall_thickness, i, dataset)
 
                 # Wenn der Loop zum ersten Mal durchläuft, wird der Wert von timestamp in der Variable time gespeichert und Timestamp auf 0 gesetzt
                 if i==0:
@@ -234,24 +264,24 @@ def convert_h5_to_json(file_path):
                     try:
                         velocity=(int(distance)*1000)/float(timestamp)
                     except:
-                        distance=int.from_bytes(distance)
+                        distance=int.from_bytes(distance, byteorder='little', signed=True)
                         velocity=(int(distance)*1000)/float(timestamp)
                 else:
                     velocity=0
-            # Datensätze in die Liste einfügen
-            new_json_data.append({
-                "punkt": i,
-                "defect_channel": defect_channel,
-                "distance": distance,
-                "magnetization": magnetization,
-                "timestamp": timestamp,
-                "velocity": velocity,
-                "wall_thickness": wall_thickness,
-            })
-            i+=1 # Zahl der punkt erhöhen
+                # Datensätze in die Liste einfügen
+                new_json_data.append({
+                    "punkt": i,
+                    "defect_channel": defect_channel,
+                    "distance": distance,
+                    "magnetization": abs(magnetization),
+                    "timestamp": timestamp,
+                    "velocity": velocity,
+                    "wall_thickness": wall_thickness,
+                })
+                i+=1 # Zahl der punkt erhöhen
             
-
         magnetization_values = [data_point['magnetization'] for data_point in new_json_data]
+        
 
         try:
             # Anwendung der linearen Regression Detrending
@@ -278,7 +308,8 @@ def prep_file(file):
 def prep_folder(folder):
     i=1          
     for file in os.listdir(folder): # Alle Dateien im Ordner durchsuchen
-        print(i)
+        if i == 2000:
+            print("Maximum number of files reached")
         i+=1
         # Wenn die Datei eine .h5 Datei ist, wird die Funktion convert_h5_to_json() aufgerufen
         if file.endswith(".h5"):            
