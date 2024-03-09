2ac1c1b09f4b65e61b6186df81a3f095b821fbb0
Tamas <lukacstamas1114@gmail.com>
2024-03-09 23:52:42 +0100

    Erweiterte Datenbereinigung, Aussreiser werden ebenfalls erkannt und durch den Median ersetzt

--- data_prep.py
+++ data_prep.py
@@ -8,100 +8,23 @@ Die .json-Dateien werden dann in der Datenbank hochgeladen.
 
 # Import Bibliotheken
 import h5py
-import json
 import base64
 import numpy as np
 import os
+import math
 from sklearn.linear_model import LinearRegression
-import re
 from itertools import islice
 from datetime import datetime
-import struct
-import math
+
 
 # Import Funktionen aus anderen Modulen
 from database import upload_file
 
+""" --- Funktionen, um Datenpunkte zu bereinigen ---"""
 # Funktion um die Datensätze in eine Liste umzuwandeln
 def convert_dataset_to_list(dataset):
     data = dataset[:]
     return list(data)
-
-def convert_data_types(distance, magnetization,velocity,timestamp,wall_thickness, i, dataset):
-    try:
-        if isinstance(distance, bytes):
-            try: 
-                distance=float(distance.decode('utf-8'))
-            except:
-                distance=dataset['distance'][i-1]
-                distance=float(distance.decode('utf-8'))
-    except (ValueError, OverflowError):
-        print(distance)
-    try:
-        if math.isnan(distance):
-            distance=dataset['distance'][i-1]
-            distance=float(distance.decode('utf-8'))
-    except:
-        pass
-    try:
-        if isinstance(magnetization, bytes):
-            try:
-                magnetization=float(magnetization.decode('utf-8'))
-            except:
-                magnetization=dataset['magnetization'][i-1]
-                magnetization=float(magnetization.decode('utf-8'))
-    except:
-        pass
-    try:
-        if math.isnan(magnetization):
-            magnetization=dataset['magnetization'][i-1]
-            magnetization=float(magnetization.decode('utf-8'))
-    except:
-        pass
-    try:
-        if isinstance(velocity, bytes):
-            try:
-                velocity=float(velocity.decode('utf-8'))
-            except:
-                velocity=dataset['velocity'][i-1]
-                velocity=float(velocity.decode('utf-8'))
-    except:
-        pass
-    try:
-        if math.isnan(velocity):
-            velocity=dataset['velocity'][i-1]
-            velocity=float(velocity.decode('utf-8'))
-    except:
-        pass
-    try:
-        if isinstance(wall_thickness, bytes):
-            try:
-                wall_thickness=float(wall_thickness.decode('utf-8'))
-            except:
-                wall_thickness=dataset['wall_thickness'][i-1]
-                wall_thickness=float(wall_thickness.decode('utf-8'))
-    except:
-        pass
-    try:
-        if math.isnan(wall_thickness):
-            wall_thickness=dataset['wall_thickness'][i-1]
-            wall_thickness=float(wall_thickness.decode('utf-8'))
-    except:
-        pass
-
-    # Versuch, den Timestamp zu dekodieren und in einen float umzuwandeln
-    try:
-        timestamp = timestamp.decode()
-    except:
-        pass
-
-    # Wenn der Timestamp ein String ist, wird er in ein datetime-Objekt umgewandelt und dann in einen Timestamp
-    try:
-        timestamp = datetime.strptime(str(timestamp), "%Y-%m-%dT%H:%M:%S")
-        timestamp = timestamp.timestamp()
-    except:
-        pass
-    return distance, magnetization,timestamp
 
 # Funktion um die Bytes Datensätze in zu encodierte JSON-Strings umzuwandeln
 def convert_dataset_bytes_to_json(dataset):
@@ -110,6 +33,73 @@ def convert_dataset_bytes_to_json(dataset):
     base64_str = f'data:application/octet-stream;base64,{encoded_data.decode("utf-8")}'
     return base64_str
 
+# Funktion um die Bytes Datensätze in zu encodierte float umzuwandeln
+def convert_data_types(datapoint, datapoint_1):
+    try:
+        if isinstance(datapoint, bytes):
+            try: 
+                datapoint=float(datapoint.decode('utf-8'))
+            except:
+                datapoint=datapoint_1
+                datapoint=float(datapoint.decode('utf-8'))
+    except:
+        pass
+    try:
+        if math.isnan(datapoint):
+            datapoint=datapoint_1
+            datapoint=float(datapoint.decode('utf-8'))
+    except:
+        pass
+    return datapoint
+
+# Funktion um die Zeitstempel zu bereinigen
+def convert_timestamp(timestamp, time, i):
+    # Versuch, den Timestamp zu dekodieren und in einen float umzuwandeln
+    try:
+        timestamp = timestamp.decode()
+    except:
+        pass
+    # Wenn der Timestamp ein String ist, wird er in ein datetime-Objekt umgewandelt und dann in einen Timestamp
+    try:
+        timestamp = datetime.strptime(str(timestamp), "%Y-%m-%dT%H:%M:%S")
+        timestamp = timestamp.timestamp()
+    except:
+        pass
+    # Wenn der Loop zum ersten Mal durchläuft, wird der Wert von timestamp in der Variable time gespeichert und Timestamp auf 0 gesetzt
+    if i==0:
+        time=timestamp
+        timestamp=0 
+    # Versuche Zietpunkt des Datensatzes zu berechnen
+    if timestamp!=0:
+        timestamp=float(timestamp)-float(time) # Zeitpunkt des Datensatzes berechnen      
+    return timestamp, time
+
+# Funktion um die Outliers in den Datensätzen zu bereinigen
+def replace_outliers_with_median(id,data):
+  # Quartile berechnen
+    try:
+        Q1 = np.percentile(data, 25)
+        Q3 = np.percentile(data, 75)
+    except:
+        print(id)
+        return data
+    # IQR ausrechnen
+    IQR = Q3 - Q1
+
+    # Definieren der unteren und oberen Grenze
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Replace outliers with median
+    median_value = np.median(data)
+    modified_data = []
+    for val in data:
+        if val < lower_bound or val > upper_bound:
+            val = min(max(val, lower_bound), upper_bound)
+        modified_data.append(val)
+    return modified_data
+
+""" --- Funktionen, um die Magnetisierungswerte zu bereinigen ---"""
 #Applies linear regression to detrend the given data.
 def linear_regression_detrend(data):
     n = len(data)
@@ -127,12 +117,12 @@ def linear_regression_detrend(data):
         adjustement = original_start_value_avg - notadjusted[0]
     return y - trend + adjustement
 
+""" --- Funktionen, um die .h5-Dateien in .json-Dateien umzuwandeln ---"""
 # Funktion um die .h5-Dateien in .json-Dateien umzuwandeln
 def convert_h5_to_json(file_path):
     # Öffnen der .h5-Datei
     with h5py.File(file_path) as f:
         data=None
-
         # Versuch, die Gruppe "data" in der Datei zu öffnen
         try:
             data = f['data']
@@ -147,7 +137,7 @@ def convert_h5_to_json(file_path):
             
         # JSON-Objekt erstellen
         json_data = {}
-
+        data_id = None
         # Attribute der Gruppe in ein Dictionary umwandeln
         group_attributes = {}
         for attr_name, attr_value in data.attrs.items(): # Attribute in der Gruppe durchgehen
@@ -169,6 +159,9 @@ def convert_h5_to_json(file_path):
             # Wenn der Datensatz Bytes enthält, wird die Funktion convert_dataset_bytes_to_json() aufgerufen, um den Datensatz in einen encodierten JSON-String umzuwandeln
             if isinstance(obj, h5py.Dataset) and obj.dtype.char == 'b':
                 dataset[name] = convert_dataset_bytes_to_json(obj)
+                # Wenn der Datensatz keine 1000 Punkte enthält, wird die Anzahl der Punkte in der Variable anz gespeichert
+                if  int(obj.shape[0]) != 1000:
+                    anz=int(obj.shape[0])
             else:
                 # Wenn der Datensatz keine 1000 Punkte enthält, wird die Anzahl der Punkte in der Variable anz gespeichert
                 if  int(obj.shape[0]) != 1000:
@@ -180,52 +173,17 @@ def convert_h5_to_json(file_path):
         time=0
         j=0
         i=0
-        avg_distance=0
-        avg_magnetization=0
-        avg_velocity=0
-        avg_wall_thickness=0
-        comp_magnetization = []
         # Datensätze in ein neues Dictionary umwandeln
         for i, (defect_channel,distance, magnetization,timestamp,velocity,wall_thickness) in \
             enumerate(zip(dataset['defect_channel'], dataset['distance'], dataset['magnetization'], \
                           dataset['timestamp'], dataset['velocity'], dataset['wall_thickness'])):
-            
-            """# Wenn der Datensatz Eastereggs enthält, werden die Werte durch den Durchschnitt ersetzttry:
-            try:
-                if isinstance(distance, bytes):
-                    decoded_distance = distance.decode('utf-8')
-                    if 'Easteregg' in decoded_distance:
-                        print('Easteregg found in distance')
-                        distance = dataset['distance'][j-1]
-                if isinstance(magnetization, bytes):
-                    decoded_magnetization = magnetization.decode('utf-8')
-                    if 'Easteregg' in decoded_magnetization:
-                        print('Easteregg found in magnetization')
-                        magnetization = dataset['magnetization'][j-1]
-                if isinstance(velocity, bytes):
-                    decoded_velocity = velocity.decode('utf-8')
-                    if 'Easteregg' in decoded_velocity:
-                        print('Easteregg found in velocity')
-                        velocity = dataset['velocity'][j-1]
-                if isinstance(wall_thickness, bytes):
-                    decoded_wall_thickness = wall_thickness.decode('utf-8')
-                    if 'Easteregg' in decoded_wall_thickness:
-                        print('Easteregg found in wall_thickness')
-                        wall_thickness = dataset['wall_thickness'][j-1]
-            except Exception as e:
-                print(f"An error occurred: {e}")"""
 
-            # Wenn der Datensatz Bytes enthält, wird die Funktion convert_dataset_bytes_to_json() aufgerufen, um den Datensatz in einen encodierten JSON-String umzuwandeln
-            distance, magnetization,timestamp = convert_data_types(distance, magnetization, velocity,timestamp, wall_thickness, i, dataset)
-
-            # Wenn der Loop zum ersten Mal durchläuft, wird der Wert von timestamp in der Variable time gespeichert und Timestamp auf 0 gesetzt
-            if i==0:
-                time=timestamp
-                timestamp=0 
-
-            # Versuche Zietpunkt des Datensatzes zu berechnen
-            if timestamp!=0:
-                    timestamp=float(timestamp)-float(time) # Zeitpunkt des Datensatzes berechnen
+            # Datenpunkte bereinigen
+            distance = convert_data_types(distance, dataset['distance'][i-1] if i!=0 else 0)
+            magnetization = convert_data_types(magnetization, dataset['magnetization'][i-1] if i!=0 else 0)
+            velocity = convert_data_types(velocity, dataset['velocity'][i-1] if i!=0 else 0)
+            wall_thickness = convert_data_types(wall_thickness, dataset['wall_thickness'][i-1] if i!=0 else 0)
+            timestamp, time = convert_timestamp(timestamp, time, i)
 
             # Datensätze in die Liste einfügen
             new_json_data.append({
@@ -247,43 +205,11 @@ def convert_h5_to_json(file_path):
         if anz!=1000: # Wenn die Anzahl der Datensätze nicht 1000 beträgt, werden die fehlenden Datensätze ausgerechnet und in die Liste eingefügt
             for (defect_channel,distance, magnetization,timestamp,wall_thickness) in \
             islice(zip(dataset['defect_channel'], dataset['distance'], dataset['magnetization'], dataset['timestamp'], dataset['wall_thickness']),j,None):
-                # Wenn der Datensatz Eastereggs enthält, werden die Werte durch den Durchschnitt ersetzt
-                """try:
-                    if isinstance(distance, bytes):
-                        decoded_distance = distance.decode('utf-8')
-                        if 'Easteregg' in decoded_distance:
-                            print('Easteregg found in distance')
-                            distance = dataset['distance'][j-1]
-                    if isinstance(magnetization, bytes):
-                        decoded_magnetization = magnetization.decode('utf-8')
-                        if 'Easteregg' in decoded_magnetization:
-                            print('Easteregg found in magnetization')
-                            magnetization = dataset['magnetization'][j-1]
-                    if isinstance(velocity, bytes):
-                        decoded_velocity = velocity.decode('utf-8')
-                        if 'Easteregg' in decoded_velocity:
-                            print('Easteregg found in velocity')
-                            velocity = dataset['velocity'][j-1]
-                    if isinstance(wall_thickness, bytes):
-                        decoded_wall_thickness = wall_thickness.decode('utf-8')
-                        if 'Easteregg' in decoded_wall_thickness:
-                            print('Easteregg found in wall_thickness')
-                            wall_thickness = dataset['wall_thickness'][j-1]
-                except Exception as e:
-                    print(f"An error occurred: {e}")"""
-
-                # Wenn der Datensatz Bytes enthält, wird die Funktion convert_dataset_bytes_to_json() aufgerufen, um den Datensatz in einen encodierten JSON-String umzuwandeln
-                distance, magnetization,timestamp = convert_data_types(distance, magnetization, velocity,timestamp, wall_thickness, i, dataset)
-
-                # Wenn der Loop zum ersten Mal durchläuft, wird der Wert von timestamp in der Variable time gespeichert und Timestamp auf 0 gesetzt
-                if i==0:
-                    time=timestamp  
-                    timestamp=0 
-
-                # Versuche Zietpunkt des Datensatzes zu berechnen
-                if timestamp!=0:
-                        timestamp=float(timestamp)-float(time) # Zeitpunkt des Datensatzes berechnen
-
+                # Datenpunkte bereinigen
+                distance = convert_data_types(distance, new_json_data[i-1]['distance'] if i!=0 else 0)
+                magnetization = convert_data_types(magnetization, new_json_data[i-1]['magnetization'] if i!=0 else 0)
+                wall_thickness = convert_data_types(wall_thickness, new_json_data[i-1]['wall_thickness'] if i!=0 else 0)   
+                timestamp, time = convert_timestamp(timestamp, time, i)
                 # Fehlende Werte für die Geschwindigkeit berechnen
                 if timestamp!=0:
                     try:
@@ -304,10 +230,15 @@ def convert_h5_to_json(file_path):
                     "wall_thickness": wall_thickness,
                 })
                 i+=1 # Zahl der punkt erhöhen
-            
-        magnetization_values = [data_point['magnetization'] for data_point in new_json_data]
-        
 
+        # Bereinigen der Outliers in den Datensätzen
+        for key in ['magnetization', 'velocity', 'wall_thickness']:
+            data = replace_outliers_with_median(data_id, data=[datapoint[key] for datapoint in new_json_data])
+            for i, datapoint in enumerate(new_json_data):
+                datapoint[key] = data[i]
+        
+        # Bereinigen der Magnetisierungswerte mit linearer Regression
+        magnetization_values = [data_point['magnetization'] for data_point in new_json_data]        
         try:
             # Anwendung der linearen Regression Detrending
             compensated_magnetization = linear_regression_detrend(magnetization_values)
@@ -325,6 +256,7 @@ def convert_h5_to_json(file_path):
         json_data['data'] = new_json_data # Datensätze in das JSON-Objekt einfügen
         return data_id, json_data
 
+# Funktion um ein .h5-Datei in eine .json-Datei umzuwandeln und in die Datenbank hochzuladen
 def prep_file(file):
         data_id,json_data = convert_h5_to_json(file)      
         upload_file(data_id, json_data) # Die Funktion upload_file() aus der Datei database.py wird aufgerufen und dabei die JSON-Datei in die Datenbank hochgeladen
